16091cfca41ae285500f905bae5470daf9ed1c13
Gilad Oved <giladoved@gmail.com>
2015-04-23 14:44:29 -0500

    this app is coming together nicely

--- app/src/main/java/thoughts/oved/gilad/thoughts/MainActivity.java
+++ app/src/main/java/thoughts/oved/gilad/thoughts/MainActivity.java
@@ -1,6 +1,8 @@
 package thoughts.oved.gilad.thoughts;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -9,6 +11,8 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
@@ -32,6 +36,9 @@ public class MainActivity extends ActionBarActivity {
     private static int removedCount;
     static boolean isEmpty;
 
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
     EditText thoughtText;
     Button postThought;
 
@@ -41,8 +48,17 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.anotherlayout);
         ButterKnife.inject(this);
+
+        mCamera = getCameraInstance();
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.camera_preview);
+        frameLayout.addView(mPreview);
+        LinearLayout layoutControls = (LinearLayout) findViewById(R.id.postthoughtid);
+        layoutControls.bringToFront();
+        com.lorentzos.flingswipe.SwipeFlingAdapterView layoutControls2 = (com.lorentzos.flingswipe.SwipeFlingAdapterView) findViewById(R.id.frame);
+        layoutControls2.bringToFront();
 
         removedCount = 0;
         isEmpty = true;
@@ -53,23 +69,44 @@ public class MainActivity extends ActionBarActivity {
         postThought.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ParseObject thoughtObject = new ParseObject("Thought");
-                thoughtObject.put("thought", thoughtText.getText().toString().trim());
-                thoughtObject.put("totalVotes", 0);
-                thoughtObject.put("upVotes", 0);
-                thoughtObject.put("rating", 0.0);
-                thoughtObject.saveInBackground(new SaveCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        thoughtText.clearFocus();
-                        InputMethodManager imm = (InputMethodManager)getSystemService(
-                                Context.INPUT_METHOD_SERVICE);
-                        imm.hideSoftInputFromWindow(thoughtText.getWindowToken(), 0);
-                        thoughtText.setText("");
-                        Toast.makeText(MainActivity.this, "Thought posted at this location", Toast.LENGTH_SHORT).show();
-                        //maybe refresh not sure
-                    }
-                });
+                if (thoughtText.length() > 0) {
+                    ParseObject thoughtObject = new ParseObject("Thought");
+                    thoughtObject.put("thought", thoughtText.getText().toString().trim());
+                    thoughtObject.put("totalVotes", 0);
+                    thoughtObject.put("upVotes", 0);
+                    thoughtObject.put("rating", 0.0);
+                    thoughtObject.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            thoughtText.clearFocus();
+                            InputMethodManager imm = (InputMethodManager) getSystemService(
+                                    Context.INPUT_METHOD_SERVICE);
+                            imm.hideSoftInputFromWindow(thoughtText.getWindowToken(), 0);
+                            thoughtText.setText("");
+                            Toast.makeText(MainActivity.this, "Thought posted at this location", Toast.LENGTH_SHORT).show();
+
+                            //refresh by time
+                            ParseQuery<ParseObject> query = ParseQuery.getQuery("Thought");
+                            //query.addAscendingOrder("created_date");
+                            query.findInBackground(new FindCallback<ParseObject>() {
+                                public void done(List<ParseObject> thoughtsList, ParseException e) {
+                                    if (e == null) {
+                                        thoughtsArray.clear();
+                                        for (ParseObject thought : thoughtsList) {
+                                            thoughtsArray.add(thought);
+                                            arrayAdapter.notifyDataSetChanged();
+                                        }
+                                        if (thoughtsArray.size() == 0)
+                                            Toast.makeText(MainActivity.this, "No thoughts here...", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Log.d("error", "Error: " + e.getMessage());
+                                    }
+                                }
+                            });
+
+                        }
+                    });
+                }
             }
         });
 
@@ -99,9 +136,11 @@ public class MainActivity extends ActionBarActivity {
                         if (e == null) {
                             int totalVotes = (int)thoughtObject.getNumber("totalVotes") + 1;
                             int upVotes = (int)thoughtObject.getNumber("upVotes");
-                            double rating = upVotes/(double)totalVotes;
+                            double rating = (double)upVotes/(double)totalVotes;
+                            makeToast(MainActivity.this, "rating is " + rating);
                             thoughtObject.put("totalVotes", totalVotes);
                             thoughtObject.put("rating", rating);
+                            thoughtObject.saveInBackground();
                         }
                     }
                 });
@@ -116,7 +155,8 @@ public class MainActivity extends ActionBarActivity {
                         if (e == null) {
                             int totalVotes = (int)thoughtObject.getNumber("totalVotes") + 1;
                             int upVotes = (int)thoughtObject.getNumber("upVotes") + 1;
-                            double rating = upVotes/(double)totalVotes;
+                            double rating = (double)upVotes/(double)totalVotes;
+                            makeToast(MainActivity.this, "rating is " + rating);
                             thoughtObject.put("totalVotes", totalVotes);
                             thoughtObject.put("upVotes", upVotes);
                             thoughtObject.put("rating", rating);
@@ -137,6 +177,11 @@ public class MainActivity extends ActionBarActivity {
                 View view = flingContainer.getSelectedView();
                 view.findViewById(R.id.item_swipe_right_indicator).setAlpha(scrollProgressPercent < 0 ? -scrollProgressPercent : 0);
                 view.findViewById(R.id.item_swipe_left_indicator).setAlpha(scrollProgressPercent > 0 ? scrollProgressPercent : 0);
+                view.findViewById(R.id.helloText).setBackgroundColor(scrollProgressPercent > 0 ? Color.GREEN : Color.RED);
+                if (scrollProgressPercent > -.01 && scrollProgressPercent < .01) {
+                    Log.d("tag", "word :" + scrollProgressPercent);
+                    view.findViewById(R.id.helloText).setBackgroundColor(Color.rgb(252, 240, 173));
+                }
             }
         });
 
@@ -171,6 +216,30 @@ public class MainActivity extends ActionBarActivity {
 
     static void makeToast(Context ctx, String s){
         Toast.makeText(ctx, s, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseCamera();
+    }
+
+    public static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open();
+        }
+        catch (Exception e){
+
+        }
+        return c;
+    }
+
+    private void releaseCamera(){
+        if (mCamera != null){
+            mCamera.release();
+            mCamera = null;
+        }
     }
 
 }
